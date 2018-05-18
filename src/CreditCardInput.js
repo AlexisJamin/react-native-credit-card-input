be0e20dee376fac39d4d0af7c6b9abb5a96fd15d
AlexisJamin <alexis.jamin@outlook.com>
2018-05-18 14:45:13 +0200

    Update CreditCardInput.js

--- src/CreditCardInput.js
+++ src/CreditCardInput.js
@@ -166,7 +166,7 @@ export default class CreditCardInput extends Component {
         <KeyboardAwareScrollView 
           ref="Form"
           enableOnAndroid={true} 
-          extraScrollHeight={100}
+          extraScrollHeight={50}
           style={s.form}>
           <CCInput {...this._inputProps("number")}
             keyboardType="numeric"
